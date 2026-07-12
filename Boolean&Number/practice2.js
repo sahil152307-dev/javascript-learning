aa88b47b45647985abf8f751679ad6d94f67b85b
Sahil <sahil1523.07@gmail.com>
2026-07-12 16:39:30 +0530

    Create practice2.js

---
+++ Boolean&Number/practice2.js
@@ -0,0 +1,67 @@
+// Operator Precedence
+// Order's
+// const operation=55+5*2
+// console.log(operation)
+// // Most priority goes to parentheses then Multiplication & Division and then addition and subtraction);
+// const operate=(4+5)**2;
+// console.log(operate)
+// let a,b;
+// // a=b=6;
+// console.log(a,b)
+// console.log(a+b);
+// const expo=2**2**3;
+// console.log(expo);
+// const same=2-3+4+5;
+// console.log(same)
+// const division=45+10/5;
+// console.log(division)
+
+// Increment and Decrement Operator:
+// Postfix and Prefix;
+//  if you need the updated value right away, use prefix. If you want the current value first and you don’t care about the increment until later, go with postfix.
+
+// let i=5;
+// let loop=++i;
+// console.log(loop)
+
+// let j=6
+// console.log(j++);
+// console.log(j)
+
+// let k=6;
+// console.log(--k);
+
+// let l=7;
+// console.log(l--);
+// console.log(l);
+
+// Compound Assignment Operator's:
+let x=4;
+x+=2;
+console.log(x);
+
+let y=2;
+y-=8;
+console.log(y);
+
+let z=24;
+z*=2;
+console.log(z);
+
+let a=68;
+a/=2;
+console.log(a);
+
+let b=4;
+b**=2;
+console.log(b);
+
+let c=50;
+c%=5;
+console.log(c);
+
+
+
+
+
+
